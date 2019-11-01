1486aed0681e4de614f8245ac0498bb0958f6054
rassoll <rassoll32@gmail.com>
2019-11-01 22:11:28 +0300

    Dependency Injection

--- src/main/java/ru/rakhmedov/spring/beans/MusicPlayer.java
+++ src/main/java/ru/rakhmedov/spring/beans/MusicPlayer.java
@@ -1,20 +1,54 @@
 package ru.rakhmedov.spring.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author RakhmedovRS
  * @created 31-Oct-19
  */
 public class MusicPlayer
 {
-	private Music music;
+	private List<Music> musics = new ArrayList<>();
+	private String name;
+	private int volume;
 
-	public MusicPlayer(Music music)
+	public MusicPlayer()
 	{
-		this.music = music;
+	}
+
+	public MusicPlayer(List<Music> musics)
+	{
+		this.musics = musics;
 	}
 
 	public void playMusic()
 	{
-		System.out.println("Playing: " + music.getSong());
+		System.out.println("Player name: " + getName());
+		System.out.println("Player volume: " + getVolume());
+		for (Music music: musics)
+		{
+			System.out.println("Playing: " + music.getSong());
+		}
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getVolume()
+	{
+		return volume;
+	}
+
+	public void setVolume(int volume)
+	{
+		this.volume = volume;
 	}
 }
---
+++ src/main/java/ru/rakhmedov/spring/beans/RapMusic.java
@@ -0,0 +1,14 @@
+package ru.rakhmedov.spring.beans;
+
+/**
+ * @author RakhmedovRS
+ * @created 01-Nov-19
+ */
+public class RapMusic implements Music
+{
+	@Override
+	public String getSong()
+	{
+		return "Gangsta paradise";
+	}
+}
